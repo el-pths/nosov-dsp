5e7b121f43bb6edf0c4687cec40b4fd0c264cef5
Наташа <@192.168.0.102>
2017-10-10 21:14:29 +0300

    Corrected tabulation

--- audio-chain/src/main/java/achain/elems/SineGenerator.java
+++ audio-chain/src/main/java/achain/elems/SineGenerator.java
@@ -6,59 +6,58 @@ import java.awt.event.ActionListener;
 import javax.swing.Timer;
 
 public class SineGenerator extends ChainElement implements Runnable {
-	
-	private int time = 100; 	
-	private int amplitude = 10000; 
-	private double gz = 440;
-	
-	public byte[] bytes;
-	public int lastB = 0;
+
+    private int time = 100;
+    private int amplitude = 10000;
+    private double gz = 440;
+
+    public byte[] bytes;
+    public int lastB = 0;
 
     public void run() {
-    	bytes = new byte[(int) (SAMPLE_RATE * BYTES_PER_SAMPLE * time / 1000)];
+        bytes = new byte[(int) (SAMPLE_RATE * BYTES_PER_SAMPLE * time / 1000)];
         byte[] b = new byte[BYTES_PER_SAMPLE];
-        Timer timer = new Timer(time, new ActionListener(){
-				public void actionPerformed(ActionEvent e) {
-					generateLine();
-				}
-        		}
-        		);
+        Timer timer = new Timer(time, new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                generateLine();
+            }
+        });
         timer.start();
-		generateLine();
+        generateLine();
         while (true) {
-        	b = getBytes(BYTES_PER_SAMPLE);
+            b = getBytes(BYTES_PER_SAMPLE);
             int value = b[1];
             value <<= 8;
             value += ((int) b[0]) & 0xFF;
             follower.nextValue(value);
         }
     }
-    
-    private void generateLine() {
-	    for(int i = 0; i < (int) (SAMPLE_RATE * BYTES_PER_SAMPLE * time/1000); i += BYTES_PER_SAMPLE) {
-	   		short count = (short) (Math.sin(i / (SAMPLE_RATE / gz) * 2.0 * Math.PI) * amplitude);
-	   		bytes[i+1] = (byte) (count >> 8);
-	    	bytes[i] = (byte) (count - bytes[i+1]);
-	   	}
-	}
 
-	public void start() {
+    private void generateLine() {
+        for (int i = 0; i < (int) (SAMPLE_RATE * BYTES_PER_SAMPLE * time / 1000); i += BYTES_PER_SAMPLE) {
+            short count = (short) (Math.sin(i / (SAMPLE_RATE / gz) * 2.0 * Math.PI) * amplitude);
+            bytes[i + 1] = (byte) (count >> 8);
+            bytes[i] = (byte) (count - bytes[i + 1]);
+        }
+    }
+
+    public void start() {
         new Thread(this).start();
     }
 
-	private byte[] getBytes(int weight) {
-		byte[] b = new byte[weight];
-		for(int i = 0; i < weight; i++){
-			b[i] = bytes[i + lastB];
-		}
-		lastB += weight;
-		if(lastB >= bytes.length){
-			lastB = 0;
-		}
-		return b;
-	}
+    private byte[] getBytes(int weight) {
+        byte[] b = new byte[weight];
+        for (int i = 0; i < weight; i++) {
+            b[i] = bytes[i + lastB];
+        }
+        lastB += weight;
+        if (lastB >= bytes.length) {
+            lastB = 0;
+        }
+        return b;
+    }
 
-	public void nextValue(int value) {
+    public void nextValue(int value) {
         throw new UnsupportedOperationException("Generator needs not any next value!");
-	}
+    }
 }
