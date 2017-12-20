c4fd4a642586dbb8a425088a839974fb07f1b58b
Наташа <@192.168.67.127>
2017-12-20 15:40:46 +0300

    Changed

--- audio-chain/src/main/java/achain/Main.java
+++ audio-chain/src/main/java/achain/Main.java
@@ -6,11 +6,15 @@ public class Main {
 
     public static void main(String... args) {
         Microphone mic = new Microphone();
+        SineGenerator sin = new SineGenerator();
         Repeater rep = new Repeater();
         AmpMeter amp = new AmpMeter();
-        mic.addFollower(rep);
-        rep.addFollower(amp);
-        mic.start();
+        Playback plb = new Playback();
+        sin.addFollower(rep);
+        rep.addFollower(plb);
+        //rep.addFollower(amp);
+        sin.start();
+        //mic.start();
     }
 
 }
