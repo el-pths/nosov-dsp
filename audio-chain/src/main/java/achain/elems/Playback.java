6070b39743562b9e71ed3fa801adb503919019be
rodion <rodiongork@github.com>
2018-01-18 12:23:43 +0300

    Fixed Playback class, working

--- audio-chain/src/main/java/achain/elems/Playback.java
+++ audio-chain/src/main/java/achain/elems/Playback.java
@@ -7,37 +7,35 @@ import javax.sound.sampled.SourceDataLine;
 
 public class Playback extends ChainElement {
 
-    private int buffer = 0;
-
+    public SourceDataLine sdl;
+    
+    public Playback() {
+        sdl = openSourceLine();
+    }
+    
+    SourceDataLine openSourceLine() {
+        try {
+            AudioFormat format = new AudioFormat(
+                    SAMPLE_RATE, BYTES_PER_SAMPLE * BITS_PER_BYTE, 1, true, false);
+            SourceDataLine line = AudioSystem.getSourceDataLine(format);
+            line.open();
+            line.start();
+            return line;
+        } catch (LineUnavailableException e) {
+            throw new RuntimeException("Problem opening playback device", e);
+        }
+    }
+    
     public void addFollower(ChainElement follower) {
         throw new UnsupportedOperationException("Playback couldn't have followers for now");
     }
 
     public void nextValue(int value) {
-        buffer = value;
-        dump();
+        byte[] data = new byte[BYTES_PER_SAMPLE];
+        data[1] = (byte) ((value >> 8) & 0xFF);
+        data[0] = (byte) (value & 0xFF);
+        sdl.write(data, 0, BYTES_PER_SAMPLE);
     }
 
-    public byte[] generateLine() {
-        byte[] line = new byte[BYTES_PER_SAMPLE];
-        line[1] = (byte) ((buffer >> 8) % 0xFF);
-        line[0] = (byte) (buffer % 0xFF);
-        return line;
-    }
-
-    void dump() {
-        AudioFormat format = new AudioFormat(SAMPLE_RATE, BYTES_PER_SAMPLE * BITS_PER_BYTE, 1, true, false);
-        try {
-            SourceDataLine sdl = AudioSystem.getSourceDataLine(format);
-            sdl.open();
-            sdl.start();
-            byte[] line = new byte[BYTES_PER_SAMPLE];
-            line = generateLine();
-            sdl.write(line, 0, line.length);
-            sdl.drain();
-            sdl.stop();
-        } catch (LineUnavailableException e) {
-            e.printStackTrace();
-        }
-    }
 }
+
